ed5f6bccfa867f5af41274be2033d4ec1fbc6e50
VazFelipe <57250903+VazFelipe@users.noreply.github.com>
2022-02-11 06:00:39 -0300

    Add files via upload

--- Elshad Karimov Course/Challenge/Arrays_Lists/Challenge_arrays_lists.py
+++ Elshad Karimov Course/Challenge/Arrays_Lists/Challenge_arrays_lists.py
@@ -1,6 +1,9 @@
 # Credits to Elshad Karimov from Udemy Course
 # https://www.udemy.com/course/data-structures-and-algorithms-bootcamp-in-python/
 
+import numpy as np
+
+
 print('Part 1 - Middle function')
 
 part_1_list = [1, 2, 3, 4]
@@ -69,3 +72,82 @@ def sumDiagonal(a):
 
 
 print(sumDiagonal(part_2_2d_list))
+
+print('Part 3 - Best Scores')
+
+part_3_best_scores_list = [84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0]
+
+print("Adapted from https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/")
+def FirstSecond(givenList):
+    """[summary]
+
+    Args:
+        givenList ([type]): [description]
+    """
+    temp_list = []
+    for my_number in givenList:
+        if my_number not in temp_list:
+            temp_list.append(my_number)
+            temp_list.sort(reverse=True)
+    first = temp_list[0]
+    second = temp_list[1]
+    return (first, second)
+    
+print(FirstSecond(part_3_best_scores_list))
+
+print("Another solution")
+def FirstSecond_2(givenList):
+    test_list = list(set(part_3_best_scores_list))
+    test_list.sort(reverse=True)
+    first = test_list[0]
+    second = test_list[1]
+    return (first, second)
+
+print(FirstSecond_2(part_3_best_scores_list))
+
+print("Part 4 - Missing Number")
+
+part_4_missing_number = [1, 2, 3, 4, 6]
+part_4_array_missing_number = np.array([1, 2, 3, 4, 6])
+
+def find_missing_number(mylist, listcount):
+    """
+    This line is intended for documentation.
+    """
+    list_total = max(mylist) * (max(mylist) + 1) / 2
+    list_user = sum(mylist)
+    print(list_total - list_user)
+
+find_missing_number(part_4_array_missing_number, 6)
+
+print("Part 5 - Removing Duplicates")
+
+part_5_remove_duplicates = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+
+def removeDuplicates(myList):
+    # TODO
+    tempList = []
+    for element in myList:
+        # print(element)
+        if element not in tempList:
+            tempList.append(element)
+    return tempList
+
+print(removeDuplicates(part_5_remove_duplicates))
+
+print("Part 6 - Finding pairs that the sum is equal to a given number")
+
+part_6_find_pairs = [2, 4, 3, 5, 6, -2, 4, 7, 8, 9]
+
+def pairSum(myList, sum):
+    # TODO
+    tempList = []
+    for element in range(len(myList)):
+        for nextElement in range(element + 1, len(myList)):
+            sumofpairs = myList[element] + myList[nextElement] 
+            if sumofpairs == sum:
+                pairs = str(myList[element]) + "+" + str(myList[nextElement])
+                tempList.append(pairs)
+    return tempList
+
+print(pairSum(part_6_find_pairs, 7))
